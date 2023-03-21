048f098e2ad76eb898c45ebbfdc81442e380b6de
IdanShap <112121953+IdanShap@users.noreply.github.com>
2023-03-21 23:34:52 +0200

    Update ida_stress_test.ps1

--- ida_stress_test.ps1
+++ ida_stress_test.ps1
@@ -1,9 +1,10 @@
+# PowerShell script for creating users for stress testing, managing permissions, and generating logon events
+
 # Capture error messages in a log file
 $ErrorActionPreference = "Continue"
 $ErrorLog = "error_log.txt"
 
-
-# Run this script as Administrator
+# Check if the script is running with Administrator privileges
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
     exit
@@ -14,11 +15,11 @@ if (-not (Get-Module -Name 'ActiveDirectory')) {
     Import-Module ActiveDirectory
 }
 
-# Prompt for domain name
+# Prompt for domain name and convert it to the LDAP path format
 $domainName = Read-Host "Please enter your domain name (e.g., yourdomain.com)"
 $domainPath = "DC=" + ($domainName -replace '\.', ',DC=')
 
-# Determine the highest existing user number
+# Determine the highest existing user number in the format user<number>
 $existingUsers = Get-ADUser -Filter "SamAccountName -like 'user*'" -Properties SamAccountName | Select-Object -ExpandProperty SamAccountName
 $highestUserNumber = 0
 foreach ($user in $existingUsers) {
@@ -33,10 +34,10 @@ foreach ($user in $existingUsers) {
 # Prompt for the number of additional users to create
 $numberOfUsers = Read-Host "Enter the number of users you want to create"
 
-# Set the password for all users
+# Set a common password for all users
 $password = ConvertTo-SecureString "Zubur1!" -AsPlainText -Force
 
-# Create the Stress Test OU
+# Create the Stress Test OU (Organizational Unit)
 New-ADOrganizationalUnit -Name "Stress Test" -Path $domainPath -ProtectedFromAccidentalDeletion $false
 
 $targetOU = "OU=Stress Test,$domainPath"
@@ -44,16 +45,17 @@ $targetOU = "OU=Stress Test,$domainPath"
 # Create a dedicated group for accessing administrative shares
 New-ADGroup -Name "StressTestAdmins" -GroupScope Global -Path $targetOU -Description "Group for Stress Test users with access to administrative shares"
 
-# Create additional users
+# Loop to create the specified number of users and add them to the appropriate groups
 Write-Output "Generating the users - please wait"
 for ($i = $highestUserNumber + 1; $i -le $highestUserNumber + $numberOfUsers; $i++) {
     $userName = "user$i"
     $userPrincipalName = "$userName@$domainName"
-	
-	# Set the user's home directory and profile path for Remote Desktop
+
+    # Set the user's home directory and profile path for Remote Desktop
     $homeDirectory = "\\$domainName\users\home\$userName"
     $profilePath = "\\$domainName\users\profiles\$userName"
 
+    # Create the new user and set its properties
     $newUser = New-ADUser -Name $userName `
         -SamAccountName $userName `
         -UserPrincipalName $userPrincipalName `
@@ -63,30 +65,28 @@ for ($i = $highestUserNumber + 1; $i -le $highestUserNumber + $numberOfUsers; $i
         -ChangePasswordAtLogon $false `
         -PasswordNeverExpires $true `
         -CannotChangePassword $true `
-		-ProfilePath "\DC1\c$\profiles\$userName" `
-		-HomeDrive "H" `
-		-HomeDirectory "\DC1\c$\home\$userName"`
+        -ProfilePath $profilePath `
+        -HomeDrive "H" `
+        -HomeDirectory $homeDirectory `
         -PassThru
 
-    # Add the user to the "StressTestAdmins" group
+    # Add the user to the "StressTestAdmins" and "Remote Desktop Users" groups
     Add-ADGroupMember -Identity "StressTestAdmins" -Members $newUser
-	Add-ADGroupMember -Identity "Remote Desktop Users" -Members $newUser
-	
-	
-	#for debug purpose:
-	echo "username " + $userName
-	echo "UPN " + $userPrincipalName
-	echo "pass " + $password
+    Add-ADGroupMember -Identity "Remote Desktop Users" -Members $newUser
+
+    # Display the created user's information for debugging purposes
+    Write-Output "Username: $userName"
+    Write-Output "UPN: $userPrincipalName"
+    Write-Output "Password: $(ConvertFrom-SecureString -SecureString $password -AsPlainText)"
 }
 
-# Add "StressTestAdmins" group to the "Remote Management Users" local group
+# Add the "StressTestAdmins" group to the "Remote Management Users" local group
 Add-ADGroupMember -Identity "Remote Management Users" -Members "StressTestAdmins"
-
 
 Write-Output "Successfully created $numberOfUsers additional users with the naming format user$($highestUserNumber + 1) up to user$($highestUserNumber + $numberOfUsers)."
 
 # Grant "StressTestAdmins" group access to the C:\ drive administrative share
-Write-Output "granting the StressTestAdmins group access to the C:\ drive administrative share - please wait, may take up to few minutes"
+Write-Output "Granting the StressTestAdmins group access to the C:\ drive administrative share - please wait, may take up to a few minutes"
 $folderPath = "C:\"
 $acl = Get-Acl $folderPath
 $groupIdentity = "$domainName\StressTestAdmins"
@@ -109,14 +109,13 @@ Write-Output "Successfully updated the LocalAccountTokenFilterPolicy registry se
 Write-Output "Disabling the Windows Firewall..."
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Write-Output "Successfully disabled the Windows Firewall."
-
 $proceed = Read-Host "Do you want to generate Logon events (ID 4624) for all the users? (yes/no)"
 if ($proceed -eq "yes") {
     # Get all user accounts
     $allUsers = Get-ADUser -Filter "SamAccountName -like 'user*'" -Properties SamAccountName | Select-Object -ExpandProperty SamAccountName
-	
-	# get plain password
-	$plainPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+    # Get the plain password from the secure string
+    $plainPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
     foreach ($userName in $allUsers) {
         $userPrincipalName = "$userName@$domainName"
@@ -146,27 +145,80 @@ if ($proceed -eq "yes") {
     Write-Output "Successfully generated Logon events (ID 4624) for all the users."
 }
 
+# Prompt to delete the users and the OU
+$deleteAll = Read-Host "Do you want to delete all the created users and the Stress Test OU? (yes/no)"
+if ($deleteAll -eq "yes") {
+    # Delete all users within the "Stress Test" OU
+    Write-Output "Deleting all users within the Stress Test OU..."
+    Get-ADUser -Filter * -SearchBase $targetOU | Remove-ADUser -Confirm:$false
+    Write-Output "Successfully deleted all users within the Stress Test OU."
+
+    # Remove "StressTestAdmins" group from the "Remote Management Users" local group
+    Remove-ADGroupMember -Identity "Remote Management Users" -Members "StressTestAdmins" -Confirm:$false
+
+    # Delete the "StressTestAdmins" group
+    Write-Output "Deleting the StressTestAdmins group..."
+    Get-ADGroup -Filter {Name -eq 'StressTestAdmins'} | Remove-ADGroup -Confirm:$false
+    Write-Output "Successfully deleted the StressTestAdmins group."
+
+    # Delete the "Stress Test" OU
+    Remove-ADOrganizationalUnit -Identity $targetOU -Confirm:$false
+    Write-Output "Successfully deleted the Stress Test OU."
+}$proceed = Read-Host "Do you want to generate Logon events (ID 4624) for all the users? (yes/no)"
+if ($proceed -eq "yes") {
+    # Get all user accounts
+    $allUsers = Get-ADUser -Filter "SamAccountName -like 'user*'" -Properties SamAccountName | Select-Object -ExpandProperty SamAccountName
+
+    # Get the plain password from the secure string
+    $plainPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+    foreach ($userName in $allUsers) {
+        $userPrincipalName = "$userName@$domainName"
+
+        # Generate a logon event (Event ID 4624) for each user
+        $credential = New-Object System.Management.Automation.PSCredential -ArgumentList @($userName, $password)
+
+        $scriptBlock = {
+            param($envComputerName, $userName, $userPrincipalName, $password)
+
+            # Sleep for a random number of seconds
+            Start-Sleep -Seconds (Get-Random -Minimum 1 -Maximum 5)
+
+            # Access the network share
+            try {
+                $netUseResult = net use * /delete /y
+                $netUseResult = net use \\$envComputerName\c$ /user:`'$userName`' `'$password`'
+                $netUseResult = net use * /delete /y
+            } catch {
+                Write-Error "Failed to access the network share for user ${userName}: $_"
+            }
+        }
+
+        Invoke-Command -ComputerName $env:COMPUTERNAME -ScriptBlock $scriptBlock -ArgumentList $env:COMPUTERNAME, $userName, $userPrincipalName, $password -Credential $credential
+    }
+
+    Write-Output "Successfully generated Logon events (ID 4624) for all the users."
+}
 
 # Prompt to delete the users and the OU
 $deleteAll = Read-Host "Do you want to delete all the created users and the Stress Test OU? (yes/no)"
 if ($deleteAll -eq "yes") {
-	# Delete all users within the "Stress Test" OU
-	Write-Output "Deleting all users within the Stress Test OU..."
-	Get-ADUser -Filter * -SearchBase $targetOU | Remove-ADUser -Confirm:$false
-	Write-Output "Successfully deleted all users within the Stress Test OU."
+    # Delete all users within the "Stress Test" OU
+    Write-Output "Deleting all users within the Stress Test OU..."
+    Get-ADUser -Filter * -SearchBase $targetOU | Remove-ADUser -Confirm:$false
+    Write-Output "Successfully deleted all users within the Stress Test OU."
 
-	# Remove "StressTestAdmins" group from the "Remote Management Users" local group
-	Remove-ADGroupMember -Identity "Remote Management Users" -Members "StressTestAdmins" -Confirm:$false
-	
+    # Remove "StressTestAdmins" group from the "Remote Management Users" local group
+    Remove-ADGroupMember -Identity "Remote Management Users" -Members "StressTestAdmins" -Confirm:$false
 
-	# Delete the "StressTestAdmins" group
-	Write-Output "Deleting the StressTestAdmins group..."
-	Get-ADGroup -Filter {Name -eq 'StressTestAdmins'} | Remove-ADGroup -Confirm:$false
-	Write-Output "Successfully deleted the StressTestAdmins group."
+    # Delete the "StressTestAdmins" group
+    Write-Output "Deleting the StressTestAdmins group..."
+    Get-ADGroup -Filter {Name -eq 'StressTestAdmins'} | Remove-ADGroup -Confirm:$false
+    Write-Output "Successfully deleted the StressTestAdmins group."
 
-	# Delete the "Stress Test" OU
-	Remove-ADOrganizationalUnit -Identity $targetOU -Confirm:$false
-	Write-Output "Successfully deleted the Stress Test OU."
+    # Delete the "Stress Test" OU
+    Remove-ADOrganizationalUnit -Identity $targetOU -Confirm:$false
+    Write-Output "Successfully deleted the Stress Test OU."
 }
 # Revert the LocalAccountTokenFilterPolicy registry setting
 Write-Output "Reverting the LocalAccountTokenFilterPolicy registry setting..."
@@ -199,3 +251,4 @@ if ($enableFirewall -eq "yes") {
 } else {
     Write-Output "Windows Firewall remains disabled. Please remember to enable it manually later."
 }
+
